a60773dd4f04dc74a395fdd196fccc4f5663b84a
SamratAdhikari <samratmetaladhikari@gmail.com>
2024-06-13 17:47:25 +0545

    added a new file

--- Javascript/class.08.js
+++ Javascript/class.08.js
@@ -1,3 +1,4 @@
+// ! Question 1
 const laptopList = [
   {
     name: "LOQ",
@@ -41,17 +42,17 @@ const laptopList = [
   },
 ];
 
-// 1. give 6 percent on 6:6(sale) on each laptop
+// ? 1. give 6 percent on 6:6(sale) on each laptop
 let newLaptopList = laptopList.map((item) => {
   return {
     ...item,
-    price: item.price - 0.6 * item.price,
+    price: item.price - 0.06 * item.price,
   };
 });
 
 console.log(newLaptopList);
 
-// 2. increase price of Dell laptops by 10%
+// ? 2. increase price of Dell laptops by 10%
 laptopList.forEach((laptop) => {
   if (laptop.brand === "Dell") {
     laptop.price *= 1.1;
@@ -59,11 +60,11 @@ laptopList.forEach((laptop) => {
 });
 console.log(laptopList);
 
-// 3. find all lenovo laptops
+// ? 3. find all lenovo laptops
 newLaptopList = laptopList.filter((laptop) => laptop.brand === "Lenovo");
 console.log(newLaptopList);
 
-// 4. update name of 'IdeaPad' to 'IdeaPad Ultra'
+// ? 4. update name of 'IdeaPad' to 'IdeaPad Ultra'
 newLaptopList = laptopList.map((laptop) => {
   if (laptop.name === "IdeaPad") {
     return {
@@ -74,14 +75,82 @@ newLaptopList = laptopList.map((laptop) => {
 });
 console.log(newLaptopList);
 
-// 5. get all laptops whose price is below $1050
+// ? 5. get all laptops whose price is below $1050
 newLaptopList = laptopList.filter((item) => item.price < 1050);
 console.log(newLaptopList);
 
-// find 'Zenbook 15' laptop details
+// ? find 'Zenbook 15' laptop details
 let value = laptopList.find((item) => item.name === "Zenbook 15");
 console.log(value);
 
-// 7. find sum of prices of all laptops
+// ? 7. find sum of prices of all laptops
 value = laptopList.reduce((sum, item) => sum + item.price, 0);
 console.log(value);
+
+// ! Question 2
+const ageList = [11, 18, 38, 49, 9, 8];
+
+// ? check if all users are adult or not
+let isAllAdult = true;
+ageList.forEach((age) => {
+  if (age < 18) {
+    isAllAdult = false;
+    return;
+  }
+});
+
+console.log(isAllAdult);
+
+// another method
+isAllAdult - ageList.every((age) => age >= 18);
+console.log(isAllAdult);
+
+// ? check if any person is an adult
+let isAdult = ageList.some((age) => age >= 18);
+console.log(isAdult);
+
+// ! Array destructure
+const numList = [11, 25, 47];
+
+console.log(numList[0], numList[1], numList[2]);
+
+// using destructure
+const [a, b, c] = numList;
+console.log(a, b, c);
+
+// ! Array copy
+const nations = ["Nepal", "USA", "Japan", "Australia"];
+
+// ? shallow copy
+let newNations = nations;
+newNations[0] = "NEPAL !!!";
+console.log(nations);
+
+// ? spread operator (...)
+newNations = [...nations];
+newNations[0] = "Our NEPAL !!!";
+console.log(nations);
+
+// ? for objects list, its a bit complex
+const userList = [{ name: "A" }, { name: "B" }];
+let newUserList = [...userList];
+newUserList[0].name = "a"; // for objects, its a ref copy
+console.log(userList);
+
+// deep copy
+newUserList = structuredClone(userList);
+newUserList[0].name = "Z";
+console.log(userList);
+
+// ? Rest operator (...)
+// asks as a collector for a function
+const getProduct = (...otherValueArray) => {
+  let product = otherValueArray.reduce((total, num) => {
+    total = total * num;
+    return total;
+  }, 1);
+  return product;
+};
+
+const result = getProduct(1, 2, 3, 4, 5, 6);
+console.log(result);
