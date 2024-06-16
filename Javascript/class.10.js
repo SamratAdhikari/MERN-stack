00502cd07e307d3fac196f489cebc16b9e151e26
SamratAdhikari <samratmetaladhikari@gmail.com>
2024-06-16 17:27:03 +0545

    added new file

--- Javascript/class.10.js
+++ Javascript/class.10.js
@@ -24,5 +24,57 @@ const sayHi = (count) => {
   count += 1;
   sayHi(count);
 };
-
 sayHi(0);
+
+// ! Promise
+// sth that happens in the future
+// to get result, we need to resolve promise
+
+// ? Tri-state:
+// pending
+// success
+// failure
+
+// ? Promise resolve methods:
+// call back func
+// .then .catch
+
+// async ... await (relevant today)
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    let randomNum = Math.random();
+
+    if (randomNum > 0.5) {
+      resolve(randomNum);
+    } else {
+      reject("Too small number");
+    }
+  }, 2000);
+});
+
+const getRandomNum = async () => {
+  try {
+    const result = await promise;
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+console.log("first");
+getRandomNum();
+console.log("third");
+
+// ? real world use case
+const getProductData = async () => {
+  try {
+    const res = await fetch("https://fakestoreapi.com/products");
+    const data = await res.json();
+    console.log(data);
+  } catch (error) {
+    console.log("Api hit failed");
+    console.log(error.message);
+  }
+};
+
+getProductData();
